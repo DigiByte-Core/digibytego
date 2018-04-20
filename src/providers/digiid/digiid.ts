066884b8a16a54139292838ddff3c853b91e54cb
digicontributer <esot@digibyte.io>
2018-04-20 21:51:01 +1000

    build updates

--- src/providers/digiid/digiid.ts
+++ src/providers/digiid/digiid.ts
@@ -129,7 +129,7 @@ export class DigiIDProvider {
       
           const pubKeyAddress = derived.privateKey.toAddress();
           const fullMessage = this._createMessage(signedMessage, pubKeyAddress.toString());
-          return fullMessage;
+          return resolve(fullMessage);
       });
     });
   }
@@ -149,12 +149,14 @@ export class DigiIDProvider {
   }
 
   public signMessage(): Promise<object> {
-    const wallets = this.profileProvider.getWallets();
-    const xpriv = wallets[0].credentials.xPrivKey;
-    var hdPrivateKey = this.bitcore.HDPrivateKey(xpriv);
-    return this.generateSignatureMessage(hdPrivateKey)
-      .then(msg => {
-        return msg;
+    return new Promise((resolve, reject) => {
+      const wallets = this.profileProvider.getWallets();
+      const xpriv = wallets[0].credentials.xPrivKey;
+      var hdPrivateKey = this.bitcore.HDPrivateKey(xpriv);
+      this.generateSignatureMessage(hdPrivateKey)
+        .then(msg => {
+          return resolve(msg);
+        });
       });
   }
 
@@ -171,19 +173,19 @@ export class DigiIDProvider {
       const wallets = this.profileProvider.getWallets();
       this.persistenceProvider.getDigiIdHistory(wallets[0].id)
         .then((history: any) => {
-          localHistory = JSON.parse(history) || [];
+          localHistory = history || [];
           return this.http.post(this._getCallBackURL(), msg)
         })
         .then(values => {
           obj.success = true;
           localHistory.unshift(obj);
           this.logger.info("SUCCESS: Feedback sent");
-          return this.persistenceProvider.setDigiIdHistory(wallets[0].id, JSON.stringify(localHistory));
+          return this.persistenceProvider.setDigiIdHistory(wallets[0].id, localHistory);
         })
         .catch(err => {
           localHistory.unshift(obj);
           this.logger.info("ERROR: Feedback sent anyway.");
-          return this.persistenceProvider.setDigiIdHistory(wallets[0].id, JSON.stringify(localHistory)).then(() => reject());
+          return this.persistenceProvider.setDigiIdHistory(wallets[0].id, localHistory).then(() => reject());
         });
     });
   }
